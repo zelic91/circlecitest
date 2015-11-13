53cf923c842fead61cda54d4809a66a7c46396ec
Thuong Nguyen <htnguyen@hoiio.com>
2015-11-13 09:03:35 +0700

    Initial codebase.

---
+++ app/src/main/java/com/thuongnh/circleciexample/MyApplication.java
@@ -0,0 +1,13 @@
+package com.thuongnh.circleciexample;
+
+import android.app.Application;
+
+/**
+ * Created by zelic on 11/13/15.
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
